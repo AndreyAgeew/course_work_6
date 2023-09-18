0c03a32fd1af9960f47af67d6a0234f08f5f7216
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-18 23:27:13 +0300

    add blog urls

--- skychimp/urls.py
+++ skychimp/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mailing.urls')),
     path('users/', include('users.urls')),
+    path('blog/', include('blog.urls')),
 ]
 
 if settings.DEBUG:
