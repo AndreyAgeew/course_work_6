e66a0fe74ac99556c3c87a41c597d7de90f9d78b
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-18 23:20:38 +0300

    add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    avatar = models.ImageField(upload_to='users/', null=True, blank=True, verbose_name='Аватар')
+    phone = models.CharField(max_length=35, verbose_name='Телефон')
+    country = models.CharField(max_length=35, verbose_name='Страна')
+    is_verified_email = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
