895b41771fb0630e2b936a1d0e5d5ca2c9980059
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-19 20:51:49 +0300

    add caching

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import BlogPostListView, BlogPostDeleteView, BlogPostDetailView, BlogPostUpdateView, BlogPostCreateView
+from django.views.decorators.cache import cache_page
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', BlogPostListView.as_view(), name='list'),
-    path('detail/<slug>/', BlogPostDetailView.as_view(), name='detail'),
+    path('detail/<slug>/', cache_page(60)(BlogPostDetailView.as_view()), name='detail'),
     path('create/', BlogPostCreateView.as_view(), name='create'),
     path('update/<slug>/', BlogPostUpdateView.as_view(), name='update'),
     path('delete/<slug>/', BlogPostDeleteView.as_view(), name='delete'),
