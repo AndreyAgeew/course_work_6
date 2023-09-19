49a4c5a11f5b37198a3ca06ae70bb33febd935d2
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-19 12:42:56 +0300

    update blog

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from pytils.translit import slugify
 from blog.models import BlogPost
 
 
-class BlogPostCreateView(CreateView, LoginRequiredMixin):
+class BlogPostCreateView(LoginRequiredMixin, CreateView):
     model = BlogPost
     fields = ('title', 'content', 'preview_image')
     success_url = reverse_lazy('blog:list')
@@ -19,6 +19,7 @@ class BlogPostCreateView(CreateView, LoginRequiredMixin):
     def form_valid(self, form):
         if form.is_valid():
             new_blog = form.save()
+            new_blog.owner = self.request.user
             new_blog.slug = slugify(new_blog.title)
             new_blog.save()
         return super().form_valid(form)
@@ -45,7 +46,7 @@ class BlogPostDetailView(DetailView):
         return self.object
 
 
-class BlogPostUpdateView(UpdateView, LoginRequiredMixin):
+class BlogPostUpdateView(LoginRequiredMixin, UpdateView):
     model = BlogPost
     fields = ('title', 'content', 'preview_image')
     extra_context = {
@@ -60,10 +61,10 @@ class BlogPostUpdateView(UpdateView, LoginRequiredMixin):
         return super().form_valid(form)
 
     def get_object(self, queryset=None):
-        title = self.kwargs.get('title')
+        title = super().get_object(queryset)
         blog = get_object_or_404(BlogPost, title=title)
-        if blog.owner != self.request.user and not self.request.user.is_staff:
-            print("!!!!!")
+        user_groups = [group.name for group in self.request.user.groups.all()]
+        if blog.owner != self.request.user and 'Managers' not in user_groups:
             raise Http404
         return blog
 
@@ -71,9 +72,17 @@ class BlogPostUpdateView(UpdateView, LoginRequiredMixin):
         return reverse('blog:detail', args=[self.kwargs.get('slug')])
 
 
-class BlogPostDeleteView(DeleteView, LoginRequiredMixin):
+class BlogPostDeleteView(LoginRequiredMixin, DeleteView):
     model = BlogPost
     success_url = reverse_lazy('blog:list')
     extra_context = {
         'title': 'Удаление записи:'
     }
+
+    def get_object(self, queryset=None):
+        title = super().get_object(queryset)
+        blog = get_object_or_404(BlogPost, title=title)
+        user_groups = [group.name for group in self.request.user.groups.all()]
+        if blog.owner != self.request.user and 'Managers' not in user_groups:
+            raise Http404
+        return blog
